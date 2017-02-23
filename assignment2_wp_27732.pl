8966436813e3d770c03c6afdff4f3bebb4b01ceb
begly <service.alex@begly.co.uk>
2017-02-23 13:41:42 +0000

    cleanup code

--- assignment2_wp_27732.pl
+++ assignment2_wp_27732.pl
@@ -1,19 +1,21 @@
 % Find hidden identity by repeatedly calling agent_ask_oracle(oscar,o(1),link,L)
-% find_identity(-A)
-find_identity(A):-
+
+find_identity(A) :-
+  % Initially all actors are suspects
   findall(PotentialActor, actor(PotentialActor), PotentialActors),
-  find_identity(A, PotentialActors).
+  find_identity(A, PotentialActors),!.
 
-find_identity(A, [A]).
-find_identity(A, PotentialActors):- 
-  agent_ask_oracle(oscar,o(1),link,Link),
-  test_actors(Link, PotentialActors, [], Final),
-  find_identity(A, Final).
+find_identity(Actor, [Actor]). % Return when we have one answer
+find_identity(Actor, PotentialActors) :- 
+  agent_ask_oracle(oscar, o(1), link, Link),
+  actors_with_link(Link, PotentialActors, [], ReducedPotentialActors),
+  find_identity(Actor, ReducedPotentialActors).
 
-test_actors(_, [], Final, Final).
-test_actors(Link, [Actor|PAs], PAsWithLink, Final) :-
-  wp(Actor,WT),
+% Finds list of actors containg Link from Actors list, effectivly map reduce
+actors_with_link(_, [], ActorsWithLink, ActorsWithLink). % Once list exhausted
+actors_with_link(Link, [Actor|Actors], WorkingActorsWithLink, ActorsWithLink) :-
+  wp(Actor, WT),
   findall(X, wt_link(WT, X), Links),
-  ( memberchk(Link, Links) -> test_actors(Link, PAs, [Actor|PAsWithLink], Final)
-  ; otherwise -> test_actors(Link, PAs, PAsWithLink, Final)
+  ( memberchk(Link, Links) -> actors_with_link(Link, Actors, [Actor|WorkingActorsWithLink], ActorsWithLink)
+  ; otherwise              -> actors_with_link(Link, Actors, WorkingActorsWithLink, ActorsWithLink)
   ).
